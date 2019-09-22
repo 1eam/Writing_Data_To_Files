0f2ea416e0744506414a38a2a8a8c8d27ef98e67
Esther <esther191299@gmail.com>
2019-09-22 19:48:09 +0200

    Succes !

--- src/writingdatatoclass/WritingDataToFiles.java
+++ src/writingdatatoclass/WritingDataToFiles.java
@@ -4,35 +4,30 @@ import java.io.*;
 
 public class WritingDataToFiles {
 	public static void main(String[] args) throws IOException {
-		FileWriter out = null; 
-		try{ 
-			out = new FileWriter("out.txt, true"); 
-			for(long number : FibonacciNumbers()) 
-			{ 
-				out.write(String.valueOf(number) + "\r\n"); 
-				// System.out.println(number); 
-			} 
+		FileWriter out = null;
+		try {
+			out = new FileWriter("out.txt", true);
+			for (long number : FibonacciNumbers()) {
+				out.write(String.valueOf(number) + "\r\n");
+				// System.out.println(number);
+			}
 
-		} 
-		catch(IOException e) 
-		{ 
-			System.err.println("File IO Failed."); 
-		} 
+		} catch (IOException e) {
+			System.err.println("File IO Failed.");
+		}
 
-		finally{ 
-			out.close(); 
-		} 
+		finally {
+			out.close();
+		}
 	}
 
-	private static long[] FibonacciNumbers() 
-	{ 
-		long[] fibNumbers = new long[50]; 
-		fibNumbers[0] = 0; 
-		fibNumbers[1] = 1; 
-		for(int i = 2; i < 50; i++) 
-		{ 
-			fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2]; 
-		} 
+	private static long[] FibonacciNumbers() {
+		long[] fibNumbers = new long[50];
+		fibNumbers[0] = 0;
+		fibNumbers[1] = 1;
+		for (int i = 2; i < 50; i++) {
+			fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2];
+		}
 		return fibNumbers;
 	}
 }
